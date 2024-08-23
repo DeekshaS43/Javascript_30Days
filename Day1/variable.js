af14e50caeb21ceb29d55492d7fb5745d21bc35f
Deeksha Sachan <deekshasachan9343@gmail.com>
2024-08-23 13:04:40 +0530

    day1

---
+++ Day1/variable.js
@@ -0,0 +1,48 @@
+//4
+var string="welcome w3villa";
+var boolean="true";
+var undefined;
+var villa="null";
+
+//5
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof undefined);
+console.log(typeof villa);
+
+
+//6
+
+let user1;
+let user2;
+let data3;
+let data4;
+
+//7
+let a1="hello";
+let a2="welcome";
+let a3="to";
+let a4="villa";
+
+
+//8
+let firstName = "Joong";
+let lastName = "Archen";
+let maritalStatus = "Single";
+let country = "India";
+let age =23;
+
+//9
+
+let first="Joong",
+    last="Archen",
+    Status="Single",
+    countryname="India",
+    presentage=23
+
+//10
+let myAge=22;
+let YourAge=23;
+console.log("I am",myAge, "years old");
+console.log("you are" ,YourAge, "years old");
+
